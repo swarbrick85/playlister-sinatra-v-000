b0f850bd925ab04c310d89f1f81c6ace8a5b0477
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-02-07 13:59:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   enable :sessions
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> 1170b6bf193ed19aedc0c594e49fd63cc9e09c06
 
   get '/' do
     erb :index
